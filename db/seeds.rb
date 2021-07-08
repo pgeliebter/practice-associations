b8e201e1115ac7cee61760ff3c90d51c38ecf4a6
Pinny Geliebter <pgibralter@gmail.com>
2021-07-08 11:27:38 -0400

    adds seed data from seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Speaker.create!([
+  {first_name: "bob", last_name: "Racoon", email: "bracoon@example.com"},
+  {first_name: "Jill", last_name: "West", email: "jwest@example.com"},
+  {first_name: "Big", last_name: "Shebang", email: "bshebang@example.com"}
+])
+Meeting.create!([
+  {title: "Dev Dev Dev", agenda: "10:30 start speaker 1, 11:30 speaker 2, and closing at 2:00 with speaker 3", location: "Hard Rock Casino, Hollywood, FL", time: "6/7/21"},
+  {title: "Mop Mop Mope", agenda: "10:30 start speaker 1, 11:30 speaker 2, and closing at 2:00 with speaker 3", location: "Hard Rock Casino, Hollywood, FL", time: "6/12/21"}
+])
+MeetingSpeaker.create!([
+  {speaker_id: 1, meeting_id: 1},
+  {speaker_id: 2, meeting_id: 1},
+  {speaker_id: 2, meeting_id: 2},
+  {speaker_id: 3, meeting_id: 2}
+])
